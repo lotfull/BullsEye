1728c5ead89afa40656ee69d2c53782799a4838f
Kam Lotfull <kamlotfull@gmail.com>
2017-04-09 08:05:10 +0300

    Bulls eye creating. here will be alert, slider, buttons, simple labels

---
+++ A_BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A_BullsEye
+//
+//  Created by Kam Lotfull on 09.04.17.
+//  Copyright © 2017 Kam Lotfull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
