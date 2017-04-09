9907c86aca54acc14aec6a0aaad842f69a781809
Kam Lotfull <kamlotfull@gmail.com>
2017-04-09 17:03:28 +0300

    after 75 pages of book.

--- A_BullsEye/ViewController.swift
+++ A_BullsEye/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let defaultValue = 50
+    var currentValue: Int = 0
+    var aimValue: Int = 0
+    var round: Int = 0
+    var score: Int = 0
+    var roundScore: Int = 0
+    
+    private func setValues() {
+        aimValue = 1 + Int(arc4random_uniform(100))
+        //slider.value = Float(defaultValue) // default slider value
+        currentValue = defaultValue
+        score += roundScore // increase score
+        round += 1; // increase round
+    }
+    
+    private func setLabels() {
+        aimLabel.text = String(aimValue)
+        ScoreLabel.text = String(score)
+        RoundLabel.text = String(round)
+    }
 
     override func viewDidLoad() {
+        sliderValueLabel.isHidden = true
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startOfNewRound()
+    }
+    
+    private func startOfNewRound() {
+        setValues()
+        setLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK - ALERT
+    @IBAction func showAlert() {
+        let message = "The value of slider is: \(currentValue)\nThe aim was: \(aimValue)"
+        let alert = UIAlertController(
+            title: "Bingo!",
+            message: message,
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startOfNewRound()
+    }
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lrintf(slider.value)
+        sliderValueLabel.text = String(slider.value)
+    }
+    @IBOutlet weak var aimLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet weak var RoundLabel: UILabel!
+    @IBAction func howToPlayButton(_ sender: UIButton) {
+    }
+    @IBAction func restartButton(_ sender: UIButton) {
+    }
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
 }
 
