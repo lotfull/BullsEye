ef393ad8b8a76508d1e68c2c875f7cc95e7fdeb8
Kam Lotfull <kamlotfull@gmail.com>
2017-04-10 21:11:04 +0300

    Added full responsible Quality game type. round label, good alerts, etc. Started doing highscore.

--- A_BullsEye/ViewController.swift
+++ A_BullsEye/ViewController.swift
@@ -18,34 +18,41 @@ class ViewController: UIViewController {
     var roundScore: Int = 0
     var difference: Int = 0
     var highscore: Int = 0
-    var isHighscore: Int = 0
+    var isHighscore = false
+    var gameType = true //
+    let QualityGame = true
+    let TimeGame = false
+    let QGRoundsQuantity: Int = 10
     @IBOutlet weak var aimLabel: UILabel!
     @IBOutlet weak var ScoreLabel: UILabel!
     @IBOutlet weak var RoundLabel: UILabel!
     @IBOutlet weak var sliderValueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var highscoreLabel: UILabel!
-    
-    private func setValues() {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lrintf(slider.value)
+        sliderValueLabel.text = String(slider.value)
+    }
+    @IBAction func restartButton(_ sender: UIButton) {
+        startNewGame()
+    }
+    private func resetMainValues() {
+        round = 0
+        score = 0
+        roundScore = 0
+        isHighscore = false
+    }
+    private func setRandomValues() {
         aimValue = 1 + Int(arc4random_uniform(100))
         defaultValue = (aimValue + 40 + Int(arc4random_uniform(20))) % 100
         slider.value = Float(defaultValue) // default slider value
         currentValue = defaultValue
-        score += roundScore // increase score
-        round += 1; // increase round
-        if score > highscore {
-            highscore = score
-            isHighscore += 1
-            let highscoreDefault = UserDefaults.standard
-            highscoreDefault.setValue(highscore, forKey: "highscore")
-            highscoreDefault.synchronize()
-        }
     }
-    
     private func highscoreAlert() {
-        let message = "You made a HighScore! King of the Bulls eye!"
+        let message = "\nKing of the Bull's eye!"
         let alert = UIAlertController(
-            title: "Amazing!",
+            title: "HighScore!",
             message: message,
             preferredStyle: .alert)
         let action = UIAlertAction(
@@ -55,17 +62,6 @@ class ViewController: UIViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    
-    private func setLabels() {
-        aimLabel.text = String(aimValue)
-        ScoreLabel.text = String(score)
-        RoundLabel.text = String(round)
-        if isHighscore > 0 {
-            if isHighscore == 1 { highscoreAlert() }
-            highscoreLabel.text = String(score)
-        }
-    }
-    
     private func setSliderDesign() {
         let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
         let trackLeftResizable = #imageLiteral(resourceName: "SliderTrackLeft").resizableImage(withCapInsets: insets)
@@ -73,21 +69,125 @@ class ViewController: UIViewController {
         slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
         slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
+    private func QRoundEndAlert(withAction action: UIAlertAction) {
+        let message = "The value of slider is: \(currentValue)\nThe aim was: \(aimValue)"
+        let alert = UIAlertController(
+            title: "\(roundScore) points!",
+            message: message,
+            preferredStyle: .alert)
+            alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    private func nextRoundAction() -> UIAlertAction {
+    return UIAlertAction(
+        title: "Next round",
+        style: .default,
+        handler: {
+            action in
+            self.QGameStartRound()
+        })
+
+    }
+    private func ShowGameResultsAction() -> UIAlertAction {
+        return UIAlertAction(
+            title: "Show results",
+            style: .default,
+            handler: {
+                action in
+                    if !self.isHighscore {
+                        self.QGameBadEndAlert()
+                    } else {
+                        self.QGameGoodEndAlert()
+                    }
+                    self.startNewGame()
+            })
+    }
+    private func QGameBadEndAlert() {
+        let message = "Your score \(score)\nThe score to beat: \(highscore)"
+        let alert = UIAlertController(
+            title: "Quality game over!",
+            message: message,
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "Ok",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    private func QGameGoodEndAlert() {
+        let message = "Your made new highscore: \(score)\nCongratulations!"
+        let alert = UIAlertController(
+            title: "You Rock!",
+            message: message,
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "Ok",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func MainButtonPressed() {
+        if gameType == QualityGame
+        {
+            calculateRoundScore()
+            if round == QGRoundsQuantity {
+                QRoundEndAlert(withAction: ShowGameResultsAction())
+            }
+            QRoundEndAlert(withAction: nextRoundAction())
+            score += roundScore
+            round += 1;
+            if score > highscore {
+                if !isHighscore { highscoreAlert() }
+                highscore = score
+                isHighscore = true
+                let defaults = UserDefaults.standard
+                defaults.setValue(highscore, forKey: "highscore")
+                defaults.synchronize()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         sliderValueLabel.isHidden = true
-        let highscoreDefault = UserDefaults.standard
-        if let testHighscore = highscoreDefault.value(forKey: "Highscore") {
+        let defaults = UserDefaults.standard
+        if let testHighscore = defaults.value(forKey: "highscore") {
             highscore = testHighscore as! Int
+            highscoreLabel.text = String(highscore)
         }
         setSliderDesign()
-        startOfNewRound()
+        startNewGame()
     }
     
-    private func startOfNewRound() {
-        setValues()
-        setLabels()
+    private func startNewGame() {
+        resetMainValues()
+        gameType = GameSwitch.isOn
+        if gameType == QualityGame {
+            changeHighscore(to: QualityGame)
+            QGameStartRound()
+        } else if gameType == TimeGame {
+            TGameStart()
+        }
+    }
+    
+    private func QGameStartRound() {
+        setQGameValues()
+        setQGameLabels()
+    }
+    private func setQGameValues() {
+        setRandomValues()
+    }
+    
+    private func setQGameLabels() {
+        aimLabel.text = String(aimValue)
+        ScoreLabel.text = String(score)
+        RoundLabel.text = "\(round) of \(QGRoundsQuantity)"
+        if isHighscore {
+            highscoreLabel.text = String(score)
+        }
     }
     
     private func calculateRoundScore() {
@@ -100,52 +200,36 @@ class ViewController: UIViewController {
             roundScore += 250
         }
     }
-
-    // MARK - ALERT
-    @IBAction func showAlert() {
-        calculateRoundScore()
-        let message = "The value of slider is: \(currentValue)\nThe aim was: \(aimValue)"
-        let alert = UIAlertController(
-            title: "\(roundScore) points!",
-            message: message,
-            preferredStyle: .alert)
-        let action = UIAlertAction(
-            title: "Ok",
-            style: .default,
-            handler: {
-                action in
-                self.startOfNewRound()
-            })
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
+    private func changeHighscore(to GameType: Bool) {
+        switch GameType {
+        case QualityGame:
+            print("Qiality Game highscore")
+            highscoreTypeLabel.text = "Quality Game\nHighscore:"
+        case TimeGame:
+            print("Time Game Highscore")
+            highscoreTypeLabel.text = "Time Game\nHighscore:"
+        default:
+            print("Highscore game error")
+        }
     }
-    @IBAction func sliderMoved(_ slider: UISlider) {
-        currentValue = lrintf(slider.value)
-        sliderValueLabel.text = String(slider.value)
-    }
-    @IBAction func restartButton(_ sender: UIButton) {
-        round = 0
-        score = 0
-        roundScore = 0
-        isHighscore = 0
-        startOfNewRound()
-    }
-    @IBAction func howToPlayButton(_ sender: UIButton) {
-        let message = "Show your bulls eye!\nPut slider as close as you can\nGet 100% score\nAchieve bonus"
-        let alert = UIAlertController(
-            title: "The rules",
-            message: message,
-            preferredStyle: .alert)
-        let action = UIAlertAction(
-            title: "Ok",
-            style: .default,
-            handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-
-    @IBOutlet weak var backgroundImage: UIImageView!
     
+    private func setTGameValues() {
+        
+    }
+    private func setTGameLabels() {
+        
+    }
+    private func TGameStart() {
+        setTGameValues()
+        setTGameLabels()
+    }
+    
+    @IBAction func gameSwitched(_ sender: UISwitch) {
+        startNewGame()
+    }
+    @IBOutlet weak var GameSwitch: UISwitch!
+    
+    @IBOutlet weak var highscoreTypeLabel: UILabel!
     
 }
 
